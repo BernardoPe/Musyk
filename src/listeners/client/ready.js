69c69fb34deaa142f763c375d5db1b199c01a9f6
Bernardo <tgavent3@gmail.com>
2024-08-24 15:38:48 +0100

    feat: add user count info

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -2,11 +2,25 @@ const { Events, ActivityType } = require("discord.js")
 
 module.exports = {
 	name: Events.ClientReady,
-	execute(bot) {
+	async execute(bot) {
 		bot.user.setActivity({
 			name: ".help | /help",
 			type: ActivityType.Listening,
 		})
-		console.log("Bot is Up")
+		const members = new Set()
+		let serverCount = bot.guilds.cache.size
+		bot.guilds.cache.forEach((guild) => {
+			guild.members.fetch().then((fetchedMembers) => {
+				fetchedMembers.forEach((member) => {
+					if (member.user.bot || members.has(member.id)) return
+					members.add(member.id)
+				})
+				serverCount--
+			})
+		})
+		while (serverCount > 0) {
+			await new Promise((resolve) => setTimeout(resolve, 100))
+		}
+		console.log(`Bot is ready, serving ${bot.guilds.cache.size} guild(s) with ${members.size} unique member(s)`)
 	},
 }
